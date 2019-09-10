eb1a6117e7209c04b10ddace412bbd59283aaf1a
Артур Мамедов <mamedov@itstep1.dp.ua>
2019-09-10 20:06:45 +0300

    experiment

--- main.js
+++ main.js
@@ -10,6 +10,27 @@ $(function () {
 
 
     })
+    $ul.on("click","li",function () {
+        $.get("http://pdfstep.zzz.com.ua",function (resp) {
+            alert(resp);
+        })
+
+
+    })
+    $ul.on("click","li",function () {
+        $.get("http://pdfstep.zzz.com.ua",function (resp) {
+            alert(resp);
+        })
+
+
+    })
+    $ul.on("click","li",function () {
+        $.get("http://pdfstep.zzz.com.ua",function (resp) {
+            alert(resp);
+        })
+
+
+    })
 
 
 
